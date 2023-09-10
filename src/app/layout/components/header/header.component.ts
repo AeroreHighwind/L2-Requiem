51997d9de9dfdcea9a4352a8d916e1e0855168c6
Gaston Gallegos <gaston52343@gmail.com>
2023-09-09 21:48:12 -0300

    modify header:
    added menuList, structure and base styling applicated

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-header',
@@ -8,6 +14,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+
 export class HeaderComponent {
 
+  constructor(private router: Router) { }
+
+  public menuItems: MenuItem[] = [
+    { title: 'Home', route: '/' },
+    { title: 'Status', route: '/' },
+    { title: 'Info', route: '/' },
+    { title: 'Download', route: '/' },
+  ]
+
+  onClick() {
+    this.router.navigate(['/'])
+  }
+
 }
+
+
+
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent {
 
