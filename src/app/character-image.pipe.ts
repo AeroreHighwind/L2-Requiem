056a8c5205640a4f55647a39d5873def2474f9aa
Gaston Gallegos <gaston52343@gmail.com>
2023-09-25 23:23:53 -0300

    modify scrollbar, add raceImg, optimize customPipe, tailwind Card

--- src/app/character-image.pipe.ts
+++ src/app/character-image.pipe.ts
@@ -9,16 +9,24 @@ export class CharacterImagePipe implements PipeTransform {
   transform(character: Character): string {
     if (!character) return '';
 
-    let imageUrl = '';
-
     // Determine the image URL based on race and sex
-    if (character.race === 1 && character.sex === 0) {
-      imageUrl = 'assets/maleElf.jpg';
-    } if (character.race === 1 && character.sex === 1) {
-      return imageUrl = 'assets/femaleElf.jpg';
-    }
-    // Add more conditions for other races and sexes
+    if (character.race === 0 && character.sex === 0) return 'assets/maleHuman.jpg';
+    if (character.race === 0 && character.sex === 1) return 'assets/femaleHuman.jpg';
 
-    return imageUrl;
+
+    if (character.race === 1 && character.sex === 0) return 'assets/maleElf.jpg';
+    if (character.race === 1 && character.sex === 1) return 'assets/femaleElf.jpg';
+
+    if (character.race === 2 && character.sex === 0) return 'assets/maleDarkElf.jpg';
+    if (character.race === 2 && character.sex === 1) return 'assets/femaleDarkElf.jpg';
+
+
+    if (character.race === 5 && character.sex === 0) return 'assets/maleKamael.jpg';
+    if (character.race === 5 && character.sex === 1) return 'assets/femaleKamael.jpg';
+
+    if (character.race === 6 && character.sex === 0) return 'assets/maleErtheia.jpg';
+    if (character.race === 6 && character.sex === 1) return 'assets/femaleErtheia.jpg';
+
+    return ''
   }
 }
